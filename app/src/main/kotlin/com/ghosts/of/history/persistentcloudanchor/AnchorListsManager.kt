9284d6a6766364b992a20167bb1cd8043c5c103b
Andrey Kovalevskiy <andkov18@gmail.com>
2023-02-03 16:57:44 +0100

    Added a new anchor

--- app/src/main/kotlin/com/ghosts/of/history/persistentcloudanchor/AnchorListsManager.kt
+++ app/src/main/kotlin/com/ghosts/of/history/persistentcloudanchor/AnchorListsManager.kt
@@ -1,5 +1,5 @@
 package com.ghosts.of.history.persistentcloudanchor
 
 fun getAnchorIdList(): MutableList<String> {
-    return mutableListOf("ua-889485a1b8f9e87b387a281f88b535ed", "ua-286d16dcb557ad7435e262bfe3941ea")
+    return mutableListOf("ua-889485a1b8f9e87b387a281f88b535ed", "ua-90a168b3eee5b5b0f54e6e98ca644729")
 }
